298317b3d74285ed607401ef72ef74b3700679ef
Vlad <35502476+TheDevFromKer@users.noreply.github.com>
2018-05-10 21:30:21 +0200

    Regeneration)

--- include/function.php
+++ include/function.php
@@ -8,10 +8,7 @@
 	}
 	
 	function Find_Payment($tx_id, $crypto, $price) {
-		
 		include 'settings.php';
-		
-		$buyed = false;
 		
 		if($crypto == "monero") {
 			$var = file('https://xmrchain.net/myoutputs/'.$tx_id."/$xmr/$vwk");
@@ -20,71 +17,63 @@
 					$buyed = true;
 				}
 		} else if($crypto == "ethereum") {
-			$var = file("https://api.ethplorer.io/getTxInfo/$tx_id?apiKey=freekey");
-			$var2 = explode(",", $var[0]);
-			
-			if(strstr($var2[6], $eth)) {
-				if(substr($var2[7], 8, -3) == $price) {
-					
-					$buyed = true;
-				}
-			}
+			$massive = implode('', file("https://api.ethplorer.io/getTxInfo/$tx_id?apiKey=freekey"));
+				$massive = json_decode($massive, true);
+				
+				if($massive['to']==$eth)
+					if($massive['value'] >= $price)
+						return true;
 		} else if($crypto == "bitcoin") {
-			$pr_lin = 0;
-			$sa_pr = $price*10**8;
-	
-			$var = file('https://blockchain.info/rawtx/'.$tx_id);
-			
-			for($i=10; $i != sizeof($var); $i++) {
-				if(strstr($var[$i], $btc)) {
-					$pr_lin = $i+1;
-				}
-			}
-			
-			$p_p_btc = substr($var[$pr_lin], 17, -2);
-			
-			if ($p_p_btc == $sa_pr) {
-				$buyed = true;
-			}
+			$massive = implode('', file("https://blockchain.info/rawtx/$tx_id"));
+				$massive = json_decode($massive, true);
+					
+				for($i =0; $i != count($massive['out']); $i++)
+					if($massive['out'][$i]['addr'] == $btc)
+						if($massive['out'][$i]['value'] >= $price*10**8)
+							return true;
 		} else if($crypto == "litecoin") {
-			$var = file("https://chainz.cryptoid.info/explorer/tx.raw.dws?coin=ltc&id=$tx_id&fmt.js");
-			for($i = 0; $i != sizeof($var2)-1; $i++) {
-				if(strstr($var2[$i], $ltc)) {
-					if(strstr($var2[$i-6], $price)) {
-						$buyed = true;
-					}
-				}
-			}
+			$massive = implode('', file("https://chainz.cryptoid.info/explorer/tx.raw.dws?coin=ltc&id=$tx_id&fmt.js"));
+				$massive = json_decode($massive, true);
+				
+				for($i =0; $i != count($massive['vout']); $i++)
+					if($massive['vout'][$i]['value'] >= "$price")
+						if($massive['vout'][$i]['scriptPubKey']['addresses'][0] == "$ltc")
+							return true;
 		} else if($crypto == "dash") {
-			$var = file("https://chainz.cryptoid.info/explorer/tx.raw.dws?coin=dash&id=$tx_id&fmt.js");
-			$var2 = explode(",", $var[0]);
-	
-			for($i = 0; $i != sizeof($var2)-1; $i++) {
-				if(strstr($var2[$i], $dash)) {
-					if(strstr($var2[$i-7], $price)) {
-						$buyed = true;
-					}
-				}
-			}
+			$massive = implode('', file("https://chainz.cryptoid.info/explorer/tx.raw.dws?coin=dash&id=$tx_id&fmt.js"));
+				$massive = json_decode($massive, true);
+				
+				for($i =0; $i != count($massive['vout']); $i++)
+					if($massive['vout'][$i]['value'] >= "$price")
+						if($massive['vout'][$i]['scriptPubKey']['addresses'][0] == "$dash")
+							return true;
 		} else {
 			die;
 		}
 		
-		return $buyed;
+		return false;
+	}
+
+	function cryptoID($name) {
+		$massive = implode('', file("https://api.coinmarketcap.com/v2/listings/"));
+		$massive = json_decode($massive, true);
+		
+		for($i =0; $i != count($massive['data']); $i++)
+			if($massive['data'][$i]['symbol'] == "$name")
+				return $massive['data'][$i]['id'];
+	}
+
+	function retPrice($crypto, $fiat) {
+		$c_id = cryptoID($crypto);
+		
+		$massive = implode('', file("https://api.coinmarketcap.com/v2/ticker/$c_id/?convert=$fiat"));
+		$massive = json_decode($massive, true);
+		
+		return $massive['data']['quotes']["$fiat"]['price']+$massive['data']['quotes']["$fiat"]['price']*5/100; // Add 5% to buy price	
 	}
 	
 	function Conventer($fiat,$crypto,$fiat_type) {
-		$c_p_temp = file("https://api.coinmarketcap.com/v1/ticker/$crypto/?convert=$fiat_type");
-		$c_price = 0;
-		if($fiat_type == "USD") {
-			$c_price = substr($c_p_temp[6], 22, -4);
-		} else {
-			$c_price = substr($c_p_temp[17], 22, -4);
-		}
-		$p_tmp0 = 1*$fiat/$c_price;
-		$expd = explode(".",$p_tmp0);
-		$price = $expd[0].'.'.substr($expd[1], 0, 3);
-		return $price;
+		return 1*$fiat/retPrice($crypto,$fiat_type);
 	}
 	
 	function SQL_Query($sql) {
